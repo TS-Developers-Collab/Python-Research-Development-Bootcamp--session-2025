c83c8f45c372ba1d30d35eca25040b5b3a064a3d
TechSabrii <bk83835@gmail.com>
2025-07-29 08:03:25 +0500

    Initial project upload

---
+++ Basic/home.py
@@ -0,0 +1,24 @@
+# print("Hello, World!")
+
+
+# first_name = "Abdul"
+# last_name = "Basit"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+# length_of_full_name = len(full_name)
+# print(length_of_full_name)
+
+# age = input("Enter your age: ")
+# print(f"Your age is: {age}")
+
+name = input("Enter your name: ")
+length_of_name = len(name)
+print(f"Hello, {name}!")
+print(f"Your name has {length_of_name} characters.")
+print("your name length is "+ str(length_of_name))
+
+print(abs(length_of_name))
+print(type(str(name)))
+print(type(int(name)))
+print(type(float(name)))
---
+++ Basic/index.py
@@ -0,0 +1,8 @@
+# first_name = input("Enter you first Name...!")
+# last_name = input("Enter you Last Name...!")
+name = " 123"
+
+name = round(int(name))
+
+
+print(name)
